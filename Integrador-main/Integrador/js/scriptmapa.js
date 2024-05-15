70aefc1775d3191cd442db6425ebffff706dc238
SolPomares <lamalaya1980@gmail.com>
2024-05-14 21:05:52 -0300

    agregar a la rama de sol

---
+++ Integrador-main/Integrador/js/scriptmapa.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var ubicacion = { lat:  -38.00042, lng: -57.5562};
+    var mapa = new google.maps.Map(document.getElementById("mapa"), {
+        zoom: 15,
+        center: ubicacion,
+    });
+    var marcador = new google.maps.Marker({
+        position: ubicacion,
+        map: mapa,
+    });
+}
