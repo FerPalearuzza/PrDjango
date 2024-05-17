3ab3725c34d8795f2301cfc51e545342481001f8
SolPomares <lamalaya1980@gmail.com>
2024-05-17 13:30:21 -0300

    Boton de rta

---
+++ Integrador/js/scriptmapa.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+/* este scrpt ya no sirve porque reemplazamos mapa  iframe
+function initMap() {
+=======
+/* saco esto habria que borrarlo al poner el iframefunction initMap() {
+>>>>>>> sol
+    var ubicacion = { lat:  -38.00042, lng: -57.5562};
+    var mapa = new google.maps.Map(document.getElementById("mapa"), {
+        zoom: 15,
+        center: ubicacion,
+    });
+    var marcador = new google.maps.Marker({
+        position: ubicacion,
+        map: mapa,
+    });
+} */
