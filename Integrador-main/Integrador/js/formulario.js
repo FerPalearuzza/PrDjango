70426e561932fb3aa26a5eb7c8bf9996e4d74164
SolPomares <lamalaya1980@gmail.com>
2024-05-17 14:41:44 -0300

    quito elemento boton enviar tras enviar

--- Integrador-main/Integrador/js/formulario.js
+++ Integrador-main/Integrador/js/formulario.js
@@ -7,4 +7,6 @@
 function rtaRegistrada(){
   document.getElementById('rta').style.display= 'block';
   console.log('Se envió una respuesta por el formulario.');
+  // Oculta el botón de enviar
+  document.getElementById('enviar').style.display = 'none';
 }
