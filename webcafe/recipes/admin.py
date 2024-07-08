354bd42060451b804284efbfb1f0406004263b95
FerPalearuzza <ferpalearuzza1955@gmail.com>
2024-07-08 17:30:34 -0300

    emulando los commits 92c809d y 68f63c9 de maxi

--- webcafe/recipes/admin.py
+++ webcafe/recipes/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Receta
 
+class RecipeAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated') 
+
 # Register your models here.
-admin.site.register(Receta)
+admin.site.register(Receta, RecipeAdmin)
